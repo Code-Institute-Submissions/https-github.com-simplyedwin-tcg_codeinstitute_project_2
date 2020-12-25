c1aae6b4407911f1989d4dca77eccca9d00e86fb
simplyedwin <simplyedwin@gmail.com>
2020-12-25 12:42:52 +0800

    remove data folder added getcurrentloc, map, bus stop code scripts

--- js/app.js
+++ js/app.js
@@ -1,38 +1,9 @@
 $(document).ready(function () {
-  //   function log(message) {
-  //     $("<div/>").text(message).prependTo("#log");
-  //     $("#log").attr("scrollTop", 0);
-  //   }
-  // $.widget("custom.catcomplete", $.ui.autocomplete, {
-  //   _create: function () {
-  //     this._super();
-  //     this.widget().menu(
-  //       "option",
-  //       "items"
-  //       // "> :not(.ui-autocomplete-category)"
-  //       // "> *"
-  //     );
-  //   },
-  //   _renderMenu: function (ul, items) {
-  //     var that = this,
-  //       currentCategory = "";
-  //     $.each(items, function (index, item) {
-  //       var li;
-  //       if (item.category != currentCategory) {
-  //         ul.append(
-  //           "<li class='ui-autocomplete-category'>" + item.category + "</li>"
-  //         );
-  //         currentCategory = item.category;
-  //       }
-  //       li = that._renderItemData(ul, item);
-  //       if (item.category) {
-  //         li.attr("aria-label", item.category + " : " + item.label);
-  //       }
-  //     });
-  //   },
-  // });
-
+  var userdestbscode = "waiting for user input...";
   var queryResult = [];
+  var gmllat = 1.3544;
+  var gmllong = 103.82;
+  var mapzoom = 11;
   var busstopnoapicalls = [
     0,
     500,
@@ -49,7 +20,9 @@ $(document).ready(function () {
 
   busstopnoapicalls.forEach(function (apicalls) {
     var settings = {
-      url: "http://datamall2.mytransport.sg/ltaodataservice/BusStops?$skip="+apicalls,
+      url:
+        "http://datamall2.mytransport.sg/ltaodataservice/BusStops?$skip=" +
+        apicalls,
       method: "GET",
       timeout: 0,
       headers: {
@@ -65,26 +38,55 @@ $(document).ready(function () {
       // console.log(response.value.length);
       var roadname;
       var landmark;
+      var busstopcode;
       for (var i = 0; i < response.value.length; i++) {
         roadname = response.value[i].RoadName;
         landmark = response.value[i].Description;
-        queryResult.push(`${landmark} near ${roadname}`);
+        busstopcode = response.value[i].BusStopCode;
+        queryResult.push(
+          `${landmark} near ${roadname} (Bus Stop Code: ${busstopcode})`
+        );
       }
-      console.log(queryResult);
-      // console.log(queryResult.length);
     });
   });
-  console.log(queryResult.length);
 
-    $("#txtQuery").autocomplete({
-      source: queryResult,
-      minLength: 0,
-      select: function (event, ui) {
-        console.log(ui.item.value);
-        var res = ui.item.value.split("near");
-        console.log(res[0]);
-        ui.item.value = res[0];
+  $("#txtQuery").autocomplete({
+    source: queryResult,
+    minLength: 1,
+    select: function (event, ui) {
+      console.log(ui.item.value);
+      var res = ui.item.value.split("Bus Stop Code: ");
+      ui.item.value;
+      userdestbscode = res[1].slice(0, -1);
+      console.log(userdestbscode);
+    },
+  });
+
+  console.log(userdestbscode);
+
+  // for generating map using mapbox api
+  mapboxgl.accessToken =
+    "pk.eyJ1Ijoic2ltcGx5ZWR3aW4iLCJhIjoiY2tpcmUycDI1MDZzczJ3cnh3cGx4NHZoYyJ9.h4T1J2-6QQW7-bRJZuwJrg";
+  var map = new mapboxgl.Map({
+    container: "map", // container id
+    style: "mapbox://styles/mapbox/streets-v11", // style URL
+    center: [gmllong, gmllat], // starting position [lng, lat]
+    zoom: mapzoom, // starting zoom
+  });
+  map.addControl(
+    new mapboxgl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true,
       },
-    });
-  
+      trackUserLocation: true,
+    })
+  );
+
+  // $("#txtQuery").on("focus", ()=>{
+  //   $("#searchbox").attr('class','input-group mb-3 w-75');
+  // });
+
+  // $("#txtQuery").on("blur", ()=>{
+  //   $("#searchbox").attr('class','input-group mb-3 w-50');
+  // });
 });
