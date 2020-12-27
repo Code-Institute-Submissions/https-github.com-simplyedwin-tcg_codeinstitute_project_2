ad66100096d170dddd88637b3fdf7270121ed241
simplyedwin <simplyedwin@gmail.com>
2020-12-27 09:59:46 +0800

    added to show user location only when clicked on the quey box created a map object

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 $(document).ready(function () {
   var userdestbscode = "waiting for user input...";
   var queryResult = [];
-  let gculat = 1.3544;
-  let gculong = 103.82;
-  var mapzoom = 11;
-  getmap(mapzoom, gculong, gculat);
+  let clat = 1.3544; // default latitude setting when the map is first loaded
+  let clong = 103.82; // default longtitude setting when the map is first loaded
+  var mapzoom = 11; // default zoom setting when the map is first loaded
+  var mapstart = 0; // to load the map at default page which will show the entire of Singapore land mass 
+  
+  getmap(mapstart, mapzoom, clong, clat);
 
-  // this array is declared due to pagination of the api, which only publish the first 500 apis by default
+  // this array is declared due to pagination of the api, which will only publish the first 500 apis by default
   var busstopnoapicalls = [
     0,
     500,
@@ -67,24 +69,40 @@ $(document).ready(function () {
 
   // to retrieve user location using geolocation when clicked onto the form query and update onto the map
   $("#txtQuery").click(function () {
-    if ("geolocation" in navigator) {
+    var options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    if (navigator.geolocation) {
       //check geolocation available
       //try to get user current location using getCurrentPosition() method
-      navigator.geolocation.getCurrentPosition(function (position) {
-        gculat = position.coords.latitude;
-        gculong = position.coords.longitude;
-        mapzoom = 16
-        getmap(mapzoom, gculong, gculat);
-        // console.log(`gculat: ${gculat} gculong: ${gculong}`);
-      });
+      navigator.geolocation.getCurrentPosition(
+        function (position) {
+          gculat = position.coords.latitude;
+          gculong = position.coords.longitude;
+          mapzoom = 16;
+          // let gcupulsingDot = new pulsingDot(100, 103, 128, 159, 1);
+          getmap(1, mapzoom, gculong, gculat);
+
+          // mapinteraction(mapgcu, gcupulsingDot);
+          console.log(`gculat: ${gculat} gculong: ${gculong}`);
+        },
+        error,
+        options
+      );
     } else {
       console.log("Browser doesn't support geolocation!");
     }
   });
-  console.log(`gculat: ${gculat} gculong: ${gculong}`);
 
   // function to generating map using mapbox api
-  function getmap(mapzoom, gculong, gculat) {
+  function getmap(maptype, mapzoom, gculong, gculat) {
     mapboxgl.accessToken =
       "pk.eyJ1Ijoic2ltcGx5ZWR3aW4iLCJhIjoiY2tpcmUycDI1MDZzczJ3cnh3cGx4NHZoYyJ9.h4T1J2-6QQW7-bRJZuwJrg";
     var map = new mapboxgl.Map({
@@ -94,6 +112,38 @@ $(document).ready(function () {
       zoom: mapzoom, // starting zoom
     });
 
+    let gcupulsingDot = new pulsingDot(map, 100, 103, 128, 159, 1);
+
+    // to run the below condition only when user clicked onto the textquery
+    if (maptype == 1) {
+      map.on("load", function () {
+        map.addImage("pulsing-dot", gcupulsingDot, { pixelRatio: 2 });
+
+        map.addSource("points", {
+          type: "geojson",
+          data: {
+            type: "FeatureCollection",
+            features: [
+              {
+                type: "Feature",
+                geometry: {
+                  type: "Point",
+                  coordinates: [gculong, gculat],
+                },
+              },
+            ],
+          },
+        });
+        map.addLayer({
+          id: "points",
+          type: "symbol",
+          source: "points",
+          layout: {
+            "icon-image": "pulsing-dot",
+          },
+        });
+      });
+    }
     // to retrieve user location using mapbox build in feature
     map.addControl(
       new mapboxgl.GeolocateControl({
@@ -104,11 +154,61 @@ $(document).ready(function () {
       })
     );
   }
-  // $("#txtQuery").on("focus", ()=>{
-  //   $("#searchbox").attr('class','input-group mb-3 w-75');
-  // });
 
-  // $("#txtQuery").on("blur", ()=>{
-  //   $("#searchbox").attr('class','input-group mb-3 w-50');
-  // });
+  // to create a pulsingDot object on the map
+  function pulsingDot(map, size, r, g, b, a) {
+    // this.map = map;
+    this.width = size;
+    this.height = size;
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.a = a;
+    this.data = new Uint8Array(size * size * 4);
+
+    this.onAdd = function () {
+      var canvas = document.createElement("canvas");
+      canvas.width = this.width;
+      canvas.height = this.height;
+      this.context = canvas.getContext("2d");
+    };
+
+    this.render = function () {
+      var duration = 1000;
+      var t = (performance.now() % duration) / duration;
+
+      var radius = (size / 2) * 0.3;
+      var outerRadius = (size / 2) * 0.7 * t + radius;
+      var context = this.context;
+
+      // draw outer circle
+      context.clearRect(0, 0, this.width, this.height);
+      context.beginPath();
+      context.arc(this.width / 2, this.height / 2, outerRadius, 0, Math.PI * 2);
+      a = 1 - t;
+      context.fillStyle = "rgba(" + r + "," + g + "," + b + "," + a + ")"; //"rgba(255, 200, 200," + (1 - t) + ")";
+      //context.fillStyle = "rgba(255, 200, 200," + (1 - t) + ")";
+      context.fill();
+
+      // draw inner circle
+      context.beginPath();
+      context.arc(this.width / 2, this.height / 2, radius, 0, Math.PI * 2);
+      context.fillStyle =
+        "rgba(" + r + "," + (g - g / 2) + "," + (b - b / 2) + ",1)"; //"rgba(255, 100, 100, 1)";
+      //context.fillStyle = "rgba(255, 100, 100, 1)";
+      context.strokeStyle = "white";
+      context.lineWidth = 2 + 4 * a;
+      context.fill();
+      context.stroke();
+
+      // update this image's data with data from the canvas
+      this.data = context.getImageData(0, 0, this.width, this.height).data;
+
+      // continuously repaint the map, resulting in the smooth animation of the dot
+      map.triggerRepaint();
+
+      // return `true` to let the map know that the image was updated
+      return true;
+    };
+  }
 });
