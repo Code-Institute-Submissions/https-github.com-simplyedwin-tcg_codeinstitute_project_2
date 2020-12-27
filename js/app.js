f91e4caf965b32406e58e3b9c7e5c65e9b3e2e16
simplyedwin <simplyedwin@gmail.com>
2020-12-27 21:37:15 +0800

    added query to get all json data added script to find nearest busstop

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 $(document).ready(function () {
   var userdestbscode = "waiting for user input...";
+  var usercoord;
   var queryResult = [];
+  var querydata = [];
   let clat = 1.3544; // default latitude setting when the map is first loaded
   let clong = 103.82; // default longtitude setting when the map is first loaded
   var mapzoom = 11; // default zoom setting when the map is first loaded
-  var mapstart = 0; // to load the map at default page which will show the entire of Singapore land mass 
-  
+  var mapstart = 0; // to load the map at default page which will show the entire of Singapore land mass
+  var gculat;
+  var gculong;
+
   getmap(mapstart, mapzoom, clong, clat);
 
   // this array is declared due to pagination of the api, which will only publish the first 500 apis by default
@@ -41,13 +45,19 @@ $(document).ready(function () {
       var roadname;
       var landmark;
       var busstopcode;
+      var lat;
+      var long;
       for (var i = 0; i < response.value.length; i++) {
         roadname = response.value[i].RoadName;
         landmark = response.value[i].Description;
         busstopcode = response.value[i].BusStopCode;
+        lat = response.value[i].Latitude;
+        long = response.value[i].Longitude;
         queryResult.push(
+          // `${landmark} near ${roadname} (Bus Stop Code: ${busstopcode}) - [${lat},${long}])`
           `${landmark} near ${roadname} (Bus Stop Code: ${busstopcode})`
         );
+        querydata.push(response.value[i]);
       }
     });
   });
@@ -59,13 +69,10 @@ $(document).ready(function () {
     select: function (event, ui) {
       console.log(ui.item.value);
       var res = ui.item.value.split("Bus Stop Code: ");
-      ui.item.value;
       userdestbscode = res[1].slice(0, -1);
       console.log(userdestbscode);
     },
   });
-
-  console.log(userdestbscode);
 
   // to retrieve user location using geolocation when clicked onto the form query and update onto the map
   $("#txtQuery").click(function () {
@@ -101,6 +108,42 @@ $(document).ready(function () {
     }
   });
 
+  $("#searchbutton").on("click", () => {
+    console.log(querydata.length);
+    console.log(`gculat: ${gculat} gculong: ${gculong}`);
+
+    // gculat = 1.4346393;
+    // gculong = 103.79162930000001;
+
+    for (var i = 0; i < querydata.length; i++) {
+      // if this location is within 0.1KM of the user, add it to the list
+      if (
+        distance(
+          gculat,
+          gculong,
+          querydata[i].Latitude,
+          querydata[i].Longitude,
+          "K"
+        ) <= 0.12
+      ) {
+        // mapboxgl.accessToken =
+        //   "pk.eyJ1Ijoic2ltcGx5ZWR3aW4iLCJhIjoiY2tpcmUycDI1MDZzczJ3cnh3cGx4NHZoYyJ9.h4T1J2-6QQW7-bRJZuwJrg";
+        // var map = new mapboxgl.Map({
+        //   container: "map", // container id
+        //   style: "mapbox://styles/mapbox/streets-v11", // style URL
+        //   center: [gculong, gculat], // starting position [lng, lat]
+        //   zoom: mapzoom, // starting zoom
+        // });
+        // var marker = new mapboxgl.Marker()
+        //   .setLngLat([querydata[i].Longitude, querydata[i].Latitude])
+        //   .addTo(map);
+        console.log(
+          `The nearest bus stop to your current location is ${querydata[i].Description} near ${querydata[i].RoadName} (${querydata[i].BusStopCode})`
+        );
+      }
+    }
+  });
+
   // function to generating map using mapbox api
   function getmap(maptype, mapzoom, gculong, gculat) {
     mapboxgl.accessToken =
@@ -112,7 +155,7 @@ $(document).ready(function () {
       zoom: mapzoom, // starting zoom
     });
 
-    let gcupulsingDot = new pulsingDot(map, 100, 103, 128, 159, 1);
+    let gcupulsingDot = new pulsingDot(map, 75, 75, 255, 255, 1);
 
     // to run the below condition only when user clicked onto the textquery
     if (maptype == 1) {
@@ -210,5 +253,29 @@ $(document).ready(function () {
       // return `true` to let the map know that the image was updated
       return true;
     };
+  }
+
+  // to compute the distance between 2 coordiates
+  function distance(lat1, lon1, lat2, lon2, unit) {
+    var radlat1 = (Math.PI * lat1) / 180;
+    var radlat2 = (Math.PI * lat2) / 180;
+    var theta = lon1 - lon2;
+    var radtheta = (Math.PI * theta) / 180;
+    var dist =
+      Math.sin(radlat1) * Math.sin(radlat2) +
+      Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    if (dist > 1) {
+      dist = 1;
+    }
+    dist = Math.acos(dist);
+    dist = (dist * 180) / Math.PI;
+    dist = dist * 60 * 1.1515;
+    if (unit == "K") {
+      dist = dist * 1.609344;
+    }
+    if (unit == "N") {
+      dist = dist * 0.8684;
+    }
+    return dist;
   }
 });
